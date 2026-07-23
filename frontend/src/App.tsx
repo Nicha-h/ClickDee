72f81651e5821238481926e79ca1744ccfc946f9
Nicha-h <nichahongsri@gmail.com>
2026-07-24 02:28:24 +0700

    feat: add navber wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from '@/components/navbar'
+import Home from '@/pages/home'
 
 const App = () => {
-
   return (
     <div className="w-full min-h-screen bg-gray-900">
-     
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
