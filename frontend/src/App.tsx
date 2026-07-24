64acdc0e005c3078c4e5133226a482746cdc511a
Nicha-h <nichahongsri@gmail.com>
2026-07-25 04:01:07 +0700

    fix: fix navbar not moving with scroll

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from '@/components/navbar'
 import Home from '@/pages/home'
+import Topbar from '@/components/Topbar'
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-gray-900">
+    <div className="flex h-screen w-full overflow-hidden">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="flex flex-1 flex-col overflow-y-auto">
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
